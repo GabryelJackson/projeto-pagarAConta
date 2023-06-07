e1a5e066c2c5d4e8a8d5fb40f7fa81b194c1fcb8
GabryelJackson <gabryeljackson5@gmail.com>
2023-06-07 17:44:35 -0300

    Projeto pagar a conta finished

---
+++ index.js
@@ -0,0 +1,23 @@
+function meuEscopo() {
+    let form = document.querySelector('.form');
+    let resultado = document.querySelector('.resultado');
+
+    function calcular(evento) {
+        evento.preventDefault();
+
+        let inputTotal = form.querySelector('.total');
+        let inputDesconto = form.querySelector('.desconto');
+        let total = Number(inputTotal.value);
+        let desconto = Number(inputDesconto.value)
+        console.log(total, desconto);
+
+        let resposta = total + ((desconto / 100) * total);
+        console.log(resposta);
+
+        resultado.innerHTML += `<p>O total a se pagar da conta + desconto do garçon é R$${resposta.toFixed(2)}.</p>`
+    }
+    form.addEventListener('submit', calcular);
+}
+meuEscopo();
+
+
